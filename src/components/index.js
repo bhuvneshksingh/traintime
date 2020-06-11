f01ba9cbbbe3723f74a726e2084a9a9ed969dc85
bhuvneshksingh <62349672+bhuvneshksingh@users.noreply.github.com>
2020-06-11 14:52:59 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable global-require */
+module.exports = {
+    UserCard: require('./user-card').default,
+    EventCard: require('./event-card/event-card').default,
+    Weather: require('./weather').default,
+    JoinForm: require('./join-form').default
+};
