ffe3808e1523506c36996825f873da38f83832df
bhuvneshksingh <62349672+bhuvneshksingh@users.noreply.github.com>
2020-06-11 14:31:23 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { BrowserRouter as Router } from 'react-router-dom'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+import ReduxLogger from 'redux-logger'
+import ReduxThunk from 'redux-thunk'
+
+// Material UI Theme
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import MuiTheme from './utils/muiTheme'
+
+import { rootReducer } from './store/reducers'
+
+import * as serviceWorker from './serviceWorker'
+
+import App from './app'
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk, ReduxLogger))
+
+ReactDOM.render(
+  <I18nextProvider i18n={i18n}>
+    <Provider store={store}>
+      <MuiThemeProvider muiTheme={MuiTheme}>
+        <Router>
+          <App />
+        </Router>
+      </MuiThemeProvider>
+    </Provider>
+  </I18nextProvider>,
+  document.getElementById('root')
+)
+serviceWorker.unregister()
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
